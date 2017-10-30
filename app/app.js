80b1cea973f4ea18618ded0d61b83f06bf757480
Ulisses Caon <ulissescaon@gmail.com>
2017-10-30 11:00:55 -0200

    Clean and update express boilerplate

--- app/app.js
+++ app/app.js
@@ -1,46 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path         = require('path')
+const logger       = require('morgan')
+const express      = require('express')
+const bodyParser   = require('body-parser')
+const cookieParser = require('cookie-parser')
+const favicon      = require('serve-favicon')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const {index} = require('./routes/index')
 
-var app = express();
+let app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index)
+app.use('/users', users)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+app.use((req, res, next) => {
+  let err        = new Error('Not Found')
+      err.status = 404
+  next(err)
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error   = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = {app}
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const index   = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+index.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+module.exports = index;
