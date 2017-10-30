fbdbab834e4218b026700da6d90ce5bef542ef03
Ulisses Caon <ulissescaon@gmail.com>
2017-10-30 13:23:37 -0200

    Add footer | Edit error styling | add about page | fix navbar links

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const index   = express.Router();
+const express = require('express')
+const index   = express.Router()
+
+let info = require('./../utils/info.json')
 
 index.get('/', (req, res, next) => {
-  res.render('index', { title: 'Cd-Easy' });
-});
+  res.render('index', { 
+    info,
+    title: 'Cd-Easy - Inicio'
+  })
+})
 
-module.exports = {index};
+index.get('/sobre', (req, res, next) => {
+  res.render('about', { 
+    info,
+    title: 'Cd-Easy - Sobre nós'
+  })
+})
+
+module.exports = {index}
